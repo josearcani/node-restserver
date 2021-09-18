13d2cac16432e94b8c25a5c5fbf1cb69f9007719
joseArc <juanjose.arcani2309@gmail.com>
2021-09-17 23:04:59 -0400

    User schema defined with mongoose

---
+++ models/user.js
@@ -0,0 +1,38 @@
+
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  img: {
+    type: String,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['ADMIN_ROLE', 'USER_ROLE'],
+  },
+  state: {
+    type: Boolean,
+    default: true,
+  },
+  google: {
+    type: Boolean,
+    default: false,
+  }
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
