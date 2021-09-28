6b5924cd0a0236537307ffc4cb24a9ea909f50df
joseArc <juanjose.arcani2309@gmail.com>
2021-09-28 08:12:54 -0400

    validateJWT error log commented

--- middlewares/validate-jwt.js
+++ middlewares/validate-jwt.js
@@ -47,7 +47,7 @@ const validateJWT = async (req = request, res, next) => {
 
     next();
   } catch (error) {
-    console.log(error)
+    // console.log(error)
     res.status(401).json({
       msg: 'Token not valid'
     })
