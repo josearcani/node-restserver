5d964369f4c38e407fb3a603106b25eb05bff636
joseArc <juanjose.arcani2309@gmail.com>
2021-09-27 20:24:46 -0400

    CRUD productos terminado. tarea

--- controllers/products.js
+++ controllers/products.js
@@ -3,13 +3,35 @@ const { Product, Category } = require('../models');
 
 // get products
 const getProducts = async (req, res = response) => {
-  res.json('get all')
+  
+  const { limit = 5, from = 0 } = req.query;
+  const query = { state: true };
+
+  const [ total, products ] = await Promise.all([
+    Product.countDocuments(query),
+    Product.find(query)
+      .populate('user', 'name')
+      .populate('category', 'name')
+      .skip(Number(from))
+      .limit(Number(limit)),
+  ]);
+
+  res.json({
+    total,
+    products,
+  })
 }
 
 // get product
 const getProduct = async (req, res = response) => {
-  res.json('get a product')
 
+  const { id } = req.params;
+
+  const product = await Product.findById( id )
+      .populate('user', 'name')
+      .populate('category', 'name');
+
+  res.json(product)
 }
 
 // create product
@@ -18,42 +40,52 @@ const createProduct = async (req, res = response) => {
   const {
     state,
     user,
-    available,
-    price = 0,
-    description = '',
+    ...body
   } = req.body;
 
-  const name     = req.body.name.toUpperCase();
-  const category = req.body.category.toUpperCase();
-
-  const categoryDB = await Category.findOne({ category })
-
   const data = {
-    name,
+    ...body,
+    name: body.name.toUpperCase(),
     user: req.user._id,
-    category: categoryDB._id,
-    price,
-    description,
   }
 
   const product = new Product(data);
 
   await product.save();
 
-  res.json(product);
+  res.status(201).json(product);
 
 }
 
 // update product
 const updateProduct = async (req, res = response) => {
-  res.json('update a product')
 
+  const { id } = req.params;
+  const {
+    state,
+    user,
+    ...data
+  } = req.body;
+
+  if (data.name) {
+    data.name = data.name.toUpperCase();
+  }
+
+  data.user = req.user._id;
+
+  const product = await Product.findByIdAndUpdate(id, data, { new: true });
+
+  res.json(product);
 }
 
 // delete product
 const deleteProduct = async (req, res = response) => {
-  res.json('delete a product')
+  
+  const { id } = req.params;
 
+  const product = await Product.findByIdAndUpdate(id, {state: false}, { new: true })
+
+  res.json(product)
 }
 
 module.exports = {
--- routes/products.js
+++ routes/products.js
@@ -11,12 +11,12 @@ const {
 
 const { idProductExists, productExits, categoryExists } = require('../helpers/db-validators');
 
-const { validateFields, validateJWT } = require('../middlewares');
+const { validateFields, validateJWT, isAdmin } = require('../middlewares');
 
 router.get('/', getProducts)
 
 router.get('/:id', [
-  param('id', 'it is not a avalid Id').isMongoId(),
+  param('id', 'it is not a valid Mongo ID').isMongoId(),
   check('id').custom(idProductExists),
   validateFields
 ], getProduct)
@@ -25,17 +25,26 @@ router.post('/', [
   validateJWT,
   body('name', 'A name is required').not().isEmpty(),
   body('name').custom(productExits),
-  body('category', 'It is not a valod mongo id').isMongoId(),
+  body('category', 'It is not a valid mongo id').isMongoId(),
   body('category').custom(categoryExists),  
   validateFields
 ], createProduct)
 
 router.put('/:id', [
-
+  validateJWT,
+  param('id', 'it is not a valid Mongo ID').isMongoId(),
+  check('id').custom(idProductExists),
+  body('category', 'It is not a valid mongo id').isMongoId(),
+  body('category').custom(categoryExists),  
+  validateFields
 ], updateProduct)
 
 router.delete('/:id', [
-
+  validateJWT,
+  isAdmin,
+  param('id', 'it is not a avalid Id').isMongoId(),
+  param('id').custom(idProductExists),
+  validateFields
 ], deleteProduct)
 
 module.exports = router;
